6cd88408efba18a8b8c86ce67aea70ac155df4be
larousse83 <laure.cambours@gmail.com>
2020-11-19 10:52:16 +0100

    add crud user ecole and articleEcole

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Ecole\ArticleEcole;
 use App\Entity\Ecole\Classe;
+use App\Entity\Ecole\Ecole;
 use App\Entity\Ecole\Enseignant;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -35,7 +38,10 @@ class DashboardController extends AbstractDashboardController
             yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home'),
             yield MenuItem::section( "Les Écoles", "fa fa-book-open" ),
             yield MenuItem::linkToCrud('Classes', 'fa fa-tags', Classe::class),
-            yield MenuItem::linkToCrud('Enseignants', 'fa fa-tags', Enseignant::class)
+            yield MenuItem::linkToCrud('Enseignants', 'fa fa-tags', Enseignant::class),
+            yield MenuItem::linkToCrud('Ecoles', 'fa fa-tags', Ecole::class),
+            yield MenuItem::linkToCrud('Articles', 'fa fa-tags', ArticleEcole::class),
+            yield MenuItem::linkToCrud('Utilisateurs', 'fa fa-tags', User::class)
         ];
 
         // ajouter restriction par la suite ici pour pas que n'importe qui puisse allez modifier l'importation
